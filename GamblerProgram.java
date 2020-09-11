658bacfd6513c0c6a21478b6ff9e3a8b655b1ef7
piyushsrii <piyushnamo888@gmail.com>
2020-09-11 11:06:58 +0530

    [Piyush] Modified : Generate Random Variable to check Win or loss

--- GamblerProgram.java
+++ GamblerProgram.java
@@ -4,4 +4,11 @@ public class GamblerProgram
 {
        static final int stakeMoney=100; //Initiaze the Constant Variable
        static final int betMoney=1;
+
+       public static void main(String args[])
+       {
+           int random = (int)Math.round(Math.random()*1+1);
+           System.out.println("Initialize the random varible to check win or loss");
+       }
+
 }
