169294906ab863051d28a9c62d977298deacebf5
piyushsrii <piyushnamo888@gmail.com>
2020-09-11 11:41:03 +0530

    [Piyush] Modified : check for the day win or loss

--- GamblerProgram.java
+++ GamblerProgram.java
@@ -1,14 +1,52 @@
 ###Welcome Message###
 
+import static gambler.GambleProraming.gambleGame;
+import java.util.Scanner;
+
 public class GamblerProgram
 {
        static final int stakeMoney=100; //Initiaze the Constant Variable
        static final int betMoney=1;
 
+       static int month_Loss=0; //instance variable
+       static int month_Win=0;
+
+       static Scanner scan=new Scanner(System.in);
+       public static void gamblerGame()
+       {
+          System.out.println("Enter the days");
+          int Num_of_Days=scan.nextInt();
+
+          for(int day=1;day<=Num_of_Days;day++)
+          {
+               int stakeMoney=100;
+               while(stakeMoney>50 && stakeMoney<150)
+               {
+               int random = (int)Math.round(Math.random()*1+1);
+
+                     if(random == 1)
+                     {
+                     stakeMoney++;
+                     }
+                     else
+                     {
+                     stakeMoney--; 
+                     }
+                }
+                if(stakeMoney == 50)
+                {
+                System.out.println("He Lost Money = "+stakeMoney+" Day ="+day);
+                }
+                else
+                {
+                System.out.println("He won Money = "+(stakeMoney-100)+" Day = "+day);
+                }
+           }
+      }
+
        public static void main(String args[])
        {
-           int random = (int)Math.round(Math.random()*1+1);
-           System.out.println("Initialize the random varible to check win or loss");
+           gamblerGame();
        }
 
 }
