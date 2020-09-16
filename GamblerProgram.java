15bfc08832fa7cf5fc5a71e6c7f408650b54d461
Piyushsrii <67362417+Piyushsrii@users.noreply.github.com>
2020-09-16 11:13:34 +0530

    [Piyush] Modified : Mostly same changes with Proper indentation and changes in variables in our program

--- GamblerProgram.java
+++ GamblerProgram.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 
 public class GamblerProgram
 {
-       static final int stakeMoney=100; //Initiaze the Constant Variable
-       static final int betMoney=1;
+       static final int StakeMoney=100; //Initiaze the Constant Variable
+       static final int BetMoney=1;
 
-       static int month_Loss=0; //instance variable
-       static int month_Win=0;
+       static int Month_Loss=0; //instance variable
+       static int Month_Win=0;
 
        static Scanner scan=new Scanner(System.in);
        public static void gamblerGame()
@@ -19,46 +19,46 @@ public class GamblerProgram
 
           for(int day=1;day<=Num_of_Days;day++)
           {
-               int stakeMoney=100;
-               while(stakeMoney>50 && stakeMoney<150)
+               int StakeMoney=100;
+               while(StakeMoney>50 && StakeMoney<150)
                {
-               int random = (int)Math.round(Math.random()*1+1);
+               int Random = (int)Math.round(Math.random()*1+1);
 
-                     if(random == 1)
+                     if(Random == 1)
                      {
-                     stakeMoney++;
+                     StakeMoney++;
                      }
                      else
                      {
-                     stakeMoney--; 
+                     StakeMoney--; 
                      }
                 }
-                if(stakeMoney == 50)
+                if(StakeMoney == 50)
                 {
-                System.out.println("He Lost Money = "+stakeMoney+" Day ="+day);
-                month_Loss += 50;
+                System.out.println("He Lost Money = "+StakeMoney+" Day ="+day);
+                Month_Loss += 50;
                 }
                 else
                 {
-                System.out.println("He won Money = "+(stakeMoney-100)+" Day = "+day);
+                System.out.println("He won Money = "+(StakeMoney-100)+" Day = "+day);
                 month_Win += 50;
                 }
             }
           System.out.println("<@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@>");
-          System.out.println("Maximunm Loss in Month = "+month_Loss);
-          System.out.println("Maximunm Profit in Month = "+month_Win);
+          System.out.println("Maximunm Loss in Month = "+Month_Loss);
+          System.out.println("Maximunm Profit in Month = "+Month_Win);
           System.out.println("<@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@>");
-          if(month_Loss > month_Win)
+          if(Month_Loss > Month_Win)
           {
-           System.out.println("Loss in the months"+(month_Loss - month_Win));
+           System.out.println("Loss in the months"+(Month_Loss - Month_Win));
           }
-          else if(month_Loss == month_Win)
+          else if(Month_Loss == Month_Win)
           {
-           System.out.println("Equal in the months_Win"+month_Win+"month_Loss"+ month_Loss);
+           System.out.println("Equal in the months_Win"+Month_Win+"month_Loss"+ Month_Loss);
           }
           else
           {
-           System.out.println("profit in the months"+(month_Win - month_Loss));
+           System.out.println("profit in the months"+(Month_Win - Month_Loss));
           }
       }
 
