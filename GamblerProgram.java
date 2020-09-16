cdae4d5cc8dd2590539409e0724b9432c4c58892
Piyushsrii <67362417+Piyushsrii@users.noreply.github.com>
2020-09-16 11:05:22 +0530

    [Piyush] Modified : Change into Proper indentation and changes in variables

--- GamblerProgram.java
+++ GamblerProgram.java
@@ -2,12 +2,12 @@
 
 public class GamblerProgram
 {
-       static final int stakeMoney=100; //Initiaze the Constant Variable
-       static final int betMoney=1;
+       static final int StakeMoney=100; //Initiaze the Constant Variable
+       static final int BetMoney=1;
 
        public static void main(String args[])
        {
-           int random = (int)Math.round(Math.random()*1+1);
+           int Random = (int)Math.round(Math.random()*1+1);
            System.out.println("Initialize the random varible to check win or loss");
        }
 
